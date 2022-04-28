c068f7b456930c39f73694e58c630960789476e3
marcodem11 <marcodemicheleta01@outlook.it>
2022-04-28 16:37:38 +0200

    create repo e prodotto

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__. '/prodotto.php';
+require_once __DIR__. '/carrello.php';
+require_once __DIR__. '/user.php';
+
+$prodotto1 = new prodotto(
+  'Medicinale',
+  'Collare antizecche',
+  'Antizecche',
+  'Dogsafe',
+  39.70
+);
+$prodotto2 = new prodotto(
+  'Cibo',
+  'Dog Nutrition',
+  'Croccantini',
+  'Atom',
+  17.50
+);
+$prodotto3 = new prodotto(
+  'Cuccia',
+  'Cuccia Ripiegabile',
+  'Cuccia morbida',
+  'LoveDog',
+  31.50
+);
+
+var_dump($prodotto1, $prodotto2, $prodotto3);
+
